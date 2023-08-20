334adcd2752122985816b9044300aa8fcdb525fe
gaby5u <gabrielaoloeri@gmail.com>
2023-08-20 23:48:45 +0300

    Add likes section

---
+++ aprecieri/script.js
@@ -0,0 +1,63 @@
+//----------------------------header-on-slide--------------------------------------
+
+const header = document.querySelector(".main-nav");
+
+window.onscroll = changeBackgroundOnScroll = () => {
+  const top = window.scrollY;
+  if (top >= 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+
+//------------------------------------toggle-menu----------------------------------
+
+const toggleMenu = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+const closeButton = document.querySelector(".close-button-menu-toggle");
+
+const menuToggle = () => {
+  while (toggleMenu.classList.toggle("active")) {
+    navLinks.classList.toggle("active");
+    closeButton.classList.toggle("active");
+  }
+};
+
+//------------------------------alex-katz-carousel-img-------------------------------
+
+const image = document.querySelectorAll(".image-container img");
+const dotsArtistCarousel = document.querySelectorAll(
+  ".dots-artist-carousel span"
+);
+let isActive = false;
+
+const toggleImage = (imageIndex) => {
+  for (i = 0; i < image.length; i++) {
+    if (i != imageIndex - 1) {
+      image[i].classList.remove("active-image");
+      image[i].classList.add("inactive-image");
+      dotsArtistCarousel[i].classList.remove("active-dot-artist-carousel");
+      isActive = true;
+    }
+  }
+  if (image[imageIndex - 1].classList == "inactive-image") {
+    image[imageIndex - 1].classList.remove("inactive-image");
+    image[imageIndex - 1].classList.add("active-image");
+
+    dotsArtistCarousel[imageIndex - 1].classList.add(
+      "active-dot-artist-carousel"
+    );
+    isActive = true;
+  } else {
+    image[imageIndex - 1].classList.remove("active-image");
+    image[imageIndex - 1].classList.add("inactive-image");
+    isActive = false;
+  }
+
+  if (isActive === false) {
+    dotsArtistCarousel[imageIndex - 1].classList.remove(
+      "active-dot-artist-carousel"
+    );
+  }
+};
